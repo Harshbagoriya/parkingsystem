a20899a80023ace6276d0565fcfd19a13c511c60
Harshbagoriya <predatori5hx@gmail.com>
2026-04-18 06:59:20 +0530

    Add admin reserve routes to bookings

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -3,10 +3,12 @@ const router  = express.Router()
 const ctrl    = require('../controllers/bookingsController')
 const { protect, adminOnly } = require('../middleware/auth')
 
-router.post('/',         protect, ctrl.createBooking)
-router.get('/my',        protect, ctrl.getMyBookings)
-router.get('/recent',    protect, ctrl.getRecentBookings)
-router.get('/',          protect, adminOnly, ctrl.getAllBookings)
-router.delete('/:id',    protect, ctrl.cancelBooking)
+router.post('/',                protect, ctrl.createBooking)
+router.get('/my',               protect, ctrl.getMyBookings)
+router.get('/recent',           protect, ctrl.getRecentBookings)
+router.post('/admin-reserve',   protect, adminOnly, ctrl.adminReserve)
+router.get('/admin-reserve',    protect, adminOnly, ctrl.getAdminReservations)
+router.get('/',                 protect, adminOnly, ctrl.getAllBookings)
+router.delete('/:id',           protect, ctrl.cancelBooking)
 
 module.exports = router
